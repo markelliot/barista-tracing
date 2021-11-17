c70b4b3b591079afdb0e8448b0930e929ef040a1
Mark Elliot <mark.elliot@gmail.com>
2021-11-17 17:27:14 -0500

    Add PR review workflow, periodic dependency update workflow, and update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,3 @@
 rootProject.name = "barista-tracing-root"
 
 include("barista-tracing")
-
-pluginManagement {
-    repositories {
-        maven { url = uri("https://dl.bintray.com/palantir/releases/") }
-        jcenter()
-        gradlePluginPortal()
-    }
-
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "com.palantir.consistent-versions") {
-                useModule("com.palantir.gradle.consistentversions:gradle-consistent-versions:${requested.version}")
-            }
-        }
-    }
-}
